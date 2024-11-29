5aafd1a9868a6367f15e8913898c9de519d727c8
Ravi Raizada <raviraizadagm@gmail.com>
2024-11-30 00:59:08 +0530

    Updates GFG / Leetcode - 30/11

---
+++ src/main/java/com/leetcode/essential/seventyFive/IncreasingTripletSubsequence.java
@@ -0,0 +1,35 @@
+package com.leetcode.essential.seventyFive;
+
+//https://leetcode.com/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        // Edge case: If the array has fewer than 3 elements, return false.
+        if (nums.length < 3) {
+            return false;
+        }
+
+        // Initialize two variables to track the smallest and the second smallest values.
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        // Traverse through the array.
+        for (int num : nums) {
+            // If the current number is smaller than or equal to the smallest, update `first`.
+            if (num <= first) {
+                first = num;
+            }
+            // If the current number is greater than `first` but smaller than or equal to `second`, update `second`.
+            else if (num <= second) {
+                second = num;
+            }
+            // If the current number is greater than both `first` and `second`, return true.
+            else {
+                return true;
+            }
+        }
+
+        // If no such triplet is found, return false.
+        return false;
+    }
+
+}
--- src/main/java/com/leetcode/essential/seventyFive/StringCompression.java
+++ src/main/java/com/leetcode/essential/seventyFive/StringCompression.java
@@ -1,24 +1,34 @@
 package com.leetcode.essential.seventyFive;
 
+//https://leetcode.com/problems/string-compression/submissions/1466025480/?envType=study-plan-v2&envId=leetcode-75
 public class StringCompression {
     public int compress(char[] chars) {
-        int count = 0;
-        StringBuilder result = new StringBuilder();
-        if (chars.length == 1) {
-            return 1;
-        }
-        for (int j = 0; j < chars.length; j++) {
-            var c = chars[j];
-            if (j + 1 < chars.length && c == chars[j + 1]) {
+        int writeIndex = 0; // Index to write characters into the array
+        int readIndex = 0;  // Index to read characters from the array
+
+        while (readIndex < chars.length) {
+            char currentChar = chars[readIndex];
+            int count = 0;
+
+            // Count the occurrences of the current character
+            while (readIndex < chars.length && chars[readIndex] == currentChar) {
+                readIndex++;
                 count++;
             }
-            else {
-                result.append(c).append(count);
-                count = 1;
+
+            // Write the character to the array
+            chars[writeIndex++] = currentChar;
+
+            // If the count is greater than 1, write the count as digits
+            if (count > 1) {
+                for (char digit : Integer
+                        .toString(count)
+                        .toCharArray()) {
+                    chars[writeIndex++] = digit;
+                }
             }
         }
-    System.out.println(result.toString());
-        chars = result.toString().toCharArray();
-        return chars.length;
+        return writeIndex;
     }
+
 }
